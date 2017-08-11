4374dadd235dfaf8d5032781e41fee6f6c122860
Karol Król <Karol.Krol@vimn.com>
2017-08-11 16:18:50 +0200

    initial commit

---
+++ src/main/java/kjkrol/aspectj/ContentLike.java
@@ -0,0 +1,9 @@
+package kjkrol.aspectj;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ContentLike {
+
+}
